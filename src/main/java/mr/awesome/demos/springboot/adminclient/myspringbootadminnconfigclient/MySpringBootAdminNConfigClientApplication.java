9ab66ff372b17f8f4af1fdea9424b0380b33a2eb
Arjun <>
2020-06-21 10:07:48 +0530

    Clean project commit
    Signed-off-by: Arjun
    
     Date:      Sun Jun 21 10:07:48 2020 +0530
     On branch master
    
     Changes to be committed:
    	modified:   .gitignore
    	new file:   build.gradle
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/mr/awesome/demos/springboot/adminclient/myspringbootadminnconfigclient/MySpringBootAdminNConfigClientApplication.java
    	new file:   src/main/resources/application.properties
    	new file:   src/test/java/mr/awesome/demos/springboot/adminclient/myspringbootadminnconfigclient/MySpringBootAdminNConfigClientApplicationTests.java

---
+++ src/main/java/mr/awesome/demos/springboot/adminclient/myspringbootadminnconfigclient/MySpringBootAdminNConfigClientApplication.java
@@ -0,0 +1,13 @@
+package mr.awesome.demos.springboot.adminclient.myspringbootadminnconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootAdminNConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootAdminNConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mr/awesome/demos/springboot/adminclient/myspringbootadminnconfigclient/MySpringBootAdminNConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.awesome.demos.springboot.adminclient.myspringbootadminnconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootAdminNConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
